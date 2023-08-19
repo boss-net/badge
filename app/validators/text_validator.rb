0a26977478343cad4834adbbcb8b1079dc8cefc6
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-08-19 09:12:27 +0600

    Create text_validator.rb

---
+++ app/validators/text_validator.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class TextValidator < ActiveModel::EachValidator
+  INVALID_CONTROL = /[\x01-\x08\x0b\x0c\x0e-\x1f]/.freeze
+  def text_acceptable?(value)
+    return true if value.nil?
+    return false unless value.valid_encoding?
+
+    (value =~ INVALID_CONTROL).nil?
+  end
+
+  def validate_each(record, attribute, value)
+    return if text_acceptable?(value)
+
+    record.errors.add attribute, (options[:message] ||
+                                  I18n.t('error_messages.valid_text'))
+  end
+end
