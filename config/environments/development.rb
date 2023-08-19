9a354d583468056935bba6d32005e190e883bcad
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-08-19 09:05:14 +0600

    Create development.rb

---
+++ config/environments/development.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+Rails.application.configure do
+  # Settings specified here will take precedence over those in
+  # config/application.rb.
+
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
+
+  config.public_file_server.enabled = true
+  # Do not eager load code on boot.
+  config.eager_load = false
+
+  # This replaces the quiet assets gem in Rails 5
+  config.assets.quiet = true
+
+  # Show full error reports and disable caching.
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = true
+  config.cache_store =
+    :memory_store,
+    { size: (ENV['RAILS_CACHE_SIZE'] || '128').to_i.megabytes }
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host }
+
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
+
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
+
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
+
+  # Asset digests allow you to set far-future HTTP expiration dates on all
+  # assets,
+  # yet still be able to expire them through the digest params.
+  config.assets.digest = true
+
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
+
+  config.after_initialize do
+    # The 'bullet' gem watches application queries and notifies
+    # when you should add eager loading (N+1 queries),
+    # when you're using eager loading that isn't necessary and
+    # when you should use counter cache.
+    Bullet.enable = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
+    # Bullet.alert = true
+    # Bullet.bullet_logger = true
+    # Bullet.console = true
+    # Bullet.growl = true
+    # Bullet.xmpp = { :account  => 'bullets_account@boss-net.org.org',
+    #               :password => 'bullets_password_for_boss-net.org',
+    #               :receiver => 'your_account@boss-net.org.org',
+    #               :show_online_status => true }
+    # Bullet.honeybadger = true
+    # Bullet.bugsnag = true
+    # Bullet.airbrake = true
+    # Bullet.rollbar = true
+    # Bullet.stacktrace_includes = [ 'your_gem', 'your_middleware' ]
+    # Bullet.stacktrace_excludes = [ 'their_gem', 'their_middleware' ]
+    # Bullet.slack = { webhook_url: 'http://some.slack.url', foo: 'bar' }
+  end
+end
