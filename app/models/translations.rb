c0ee405fb39131b58f27f1b74ddd41efe77d5fae
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-08-19 09:11:08 +0600

    Create translations.rb

---
+++ app/models/translations.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Translations
+  module_function
+
+  # This class is a used to gather sets of translations which will commonly
+  # be called in bulk.  For example, there are translations we would like
+  # to export to JavaScript.  We can use a method here to do that quickly.
+
+  def for_js
+    js_translations =
+      I18n.available_locales.map do |locale|
+        locale_t =
+          I18n.t('.projects.misc.in_javascript').keys.map do |k|
+            [k, I18n.t(".projects.misc.in_javascript.#{k}", locale: locale)]
+          end
+        [locale, locale_t.to_h]
+      end
+    js_translations.to_h
+  end
+end
